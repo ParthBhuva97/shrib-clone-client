59947fa9fee17089ee6cfb7b05c441fd185e6e4d
Parth <parthbhuva97@gmail.com>
2024-05-26 14:52:17 +0530

    Fixed Routing Issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Routes , Route} from "react-router-dom";
 function App() {
   return (
     <Routes>
-    <Route path="*" element={<Note />}></Route>
+    <Route path="/:id" element={<Note />}></Route>
     </Routes>    
   )
 }
